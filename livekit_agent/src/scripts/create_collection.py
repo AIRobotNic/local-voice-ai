eaae3dd8410e6959d4cc0bd23db6f4d40b503da1
Nicolai Petrov <phone.pitonic@gmail.com>
2026-01-14 17:57:27 +0200

    Refactor create_collection.py to load environment variables from .env.local and reorganize imports

--- livekit_agent/src/scripts/create_collection.py
+++ livekit_agent/src/scripts/create_collection.py
@@ -1,6 +1,10 @@
-from qdrant_client import QdrantClient
-from qdrant_client.models import VectorParams, Distance
 import os
+
+from dotenv import load_dotenv
+from qdrant_client import QdrantClient
+from qdrant_client.models import Distance, VectorParams
+
+load_dotenv(".env.local")
 
 client = QdrantClient(url=os.getenv("QDRANT_ENDPOINT", "http://localhost:6333"))
 
